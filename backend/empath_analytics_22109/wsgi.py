7a7a3bd7f6d2fd4461b47d0f5a564627cdfd436e
Crowdbotics <team@crowdbotics.com>
2020-10-30 02:33:51 +0000

    Update models

--- backend/empath_analytics_22109/wsgi.py
+++ backend/empath_analytics_22109/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'empath_analytics_22109.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "empath_analytics_22109.settings")
 
 application = get_wsgi_application()
